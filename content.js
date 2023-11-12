635f1edc8081a333e3af6a200bf28970eb7c330a
Luke Robinett <lukebootcamp@gmail.com>
2023-11-11 20:05:04 -0800

    Added labeling of ChatGPT buttons under responses for copy, like/dislike and regenerate.

--- content.js
+++ content.js
@@ -15,11 +15,49 @@ function replaceDivsWithH3() {
     });
 }
 
+function addAriaLabelsToButtons() {
+    // Select the container of all ChatGPT response items
+    const responseContainer = document.querySelector('div.flex.flex-col.text-sm.gizmo\\:pb-9.dark\\:bg-gray-800.gizmo\\:dark\\:bg-transparent');
+
+    if (responseContainer) {
+        console.log('Response container');
+        console.log(responseContainer);
+
+        // Select the last ChatGPT response item
+        const latestResponse = responseContainer.querySelector('div.w-full.text-token-text-primary.border-b.border-black\\/10.gizmo\\:border-0.dark\\:border-gray-900\\/50.gizmo\\:dark\\:border-0.bg-gray-50.gizmo\\:bg-transparent.dark\\:bg-\\[\\#444654\\].gizmo\\:dark\\:bg-transparent:last-child');
+
+        if (latestResponse) {
+            console.log('latestResponse');
+            console.log(latestResponse);
+
+            // Further query inside latestResponse to find the div containing the buttons
+            const buttonDiv = latestResponse.querySelector('div.text-gray-400.flex.self-end.lg\\:self-center.justify-center.gizmo\\:lg\\:justify-start.mt-2.gizmo\\:mt-0.visible.gap-1');
+
+            if (buttonDiv) {
+                console.log('Button Div');
+                console.log(buttonDiv);
+
+                const buttons = buttonDiv.querySelectorAll('button');
+                console.log('Buttons');
+                console.log(buttons);
+                const labels = ['Copy Response', 'Like Response', 'Dislike Response', 'Regenerate Response'];
+
+                buttons.forEach((button, index) => {
+                    if (index < labels.length && !button.hasAttribute('aria-label')) {
+                        button.setAttribute('aria-label', labels[index]);
+                    }
+                });
+            }
+        }
+    }
+}
+
 if (targetNode) {
     const observer = new MutationObserver(mutations => {
         mutations.forEach(mutation => {
             if (mutation.addedNodes.length) {
                 replaceDivsWithH3();
+                addAriaLabelsToButtons();
             }
         });
     });
@@ -28,6 +66,7 @@ if (targetNode) {
 
     observer.observe(targetNode, config);
     replaceDivsWithH3(); // Run initially for existing elements
+    addAriaLabelsToButtons();
 } else {
     console.log('Target node (main) not found');
 }

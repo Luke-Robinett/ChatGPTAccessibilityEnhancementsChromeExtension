89a4c50e770b1a2f8a9231e166a6f364a36c2f26
Luke Robinett <lukebootcamp@gmail.com>
2023-11-12 16:36:50 -0800

    Implemented labeling of buttons under GPT responses for copying, liking, disliking or regenerating. Added live region announcements for GPT starting and finishing it's responses.

--- content.js
+++ content.js
@@ -1,72 +1,154 @@
-const targetNode = document.querySelector('main');
+// In order to alert the user that GPT has finished generating it's message and label the buttons below that finished message, we use an observer to monitor the message div.
+// This constant says when to give up and disconnect the instance of the observer for that message div if GPT is taking a while to finish.
+const MAX_WAIT_TIME = 60000;
 
-function replaceDivsWithH3() {
-    if (!targetNode) return; // Exit if targetNode is not found
+function isChatMessage(node) {
+    return node.classList && node.classList.contains("w-full") && node.classList.contains("text-token-text-primary");
+}
 
-    const divs = targetNode.querySelectorAll('div.font-semibold');
+function isUserMessage(node) {
+    return isChatMessage(node) && node.querySelector('div.font-semibold') && node.querySelector('div.font-semibold').textContent.trim() === 'You';
+}
 
-    divs.forEach(div => {
-        if (div.textContent.trim() === 'ChatGPT') {
+function isChatGptMessage(node) {
+    return isChatMessage(node) && node.querySelector('div.font-semibold') && node.querySelector('div.font-semibold').textContent.trim() === 'ChatGPT';
+}
+
+function responseIsFinished(node) {
+    return node.querySelector('button[aria-label="Regenerate Response"]') !== null;
+}
+
+function replaceChatGptDivWithH3(node) {
+    // Query for all matching div elements within the provided node
+    const chatGPTDivs = node.querySelectorAll('div.font-semibold');
+
+    // Iterate through the matching divs
+    chatGPTDivs.forEach((chatGPTDiv) => {
+        if (chatGPTDiv.textContent.trim() === 'ChatGPT') {
             const h3 = document.createElement('h3');
-            h3.className = div.className;
-            h3.textContent = div.textContent;
-            div.parentNode.replaceChild(h3, div);
+            h3.className = chatGPTDiv.className;
+            h3.textContent = chatGPTDiv.textContent;
+            chatGPTDiv.parentNode.replaceChild(h3, chatGPTDiv);
+            console.log('Replaced ChatGPT div with h3.');
         }
     });
 }
 
-function addAriaLabelsToButtons() {
-    // Select the container of all ChatGPT response items
-    const responseContainer = document.querySelector('div.flex.flex-col.text-sm.gizmo\\:pb-9.dark\\:bg-gray-800.gizmo\\:dark\\:bg-transparent');
+function addAriaLabelsToButtons(node) {
+    // Query for all matching button containers within the provided node
+    const buttonDivs = node.querySelectorAll('div.text-gray-400.flex.self-end.lg\\:self-center.justify-center.gizmo\\:lg\\:justify-start.mt-2.gizmo\\:mt-0.visible.gap-1');
 
-    if (responseContainer) {
-        console.log('Response container');
-        console.log(responseContainer);
+    // Iterate through the matching button containers
+    buttonDivs.forEach((buttonDiv) => {
+        console.log('Buttons container found.');
+        const buttons = buttonDiv.querySelectorAll('button');
+        const labels = ['Copy Response', 'Like Response', 'Dislike Response', 'Regenerate Response'];
 
-        // Select the last ChatGPT response item
-        const latestResponse = responseContainer.querySelector('div.w-full.text-token-text-primary.border-b.border-black\\/10.gizmo\\:border-0.dark\\:border-gray-900\\/50.gizmo\\:dark\\:border-0.bg-gray-50.gizmo\\:bg-transparent.dark\\:bg-\\[\\#444654\\].gizmo\\:dark\\:bg-transparent:last-child');
-
-        if (latestResponse) {
-            console.log('latestResponse');
-            console.log(latestResponse);
-
-            // Further query inside latestResponse to find the div containing the buttons
-            const buttonDiv = latestResponse.querySelector('div.text-gray-400.flex.self-end.lg\\:self-center.justify-center.gizmo\\:lg\\:justify-start.mt-2.gizmo\\:mt-0.visible.gap-1');
-
-            if (buttonDiv) {
-                console.log('Button Div');
-                console.log(buttonDiv);
-
-                const buttons = buttonDiv.querySelectorAll('button');
-                console.log('Buttons');
-                console.log(buttons);
-                const labels = ['Copy Response', 'Like Response', 'Dislike Response', 'Regenerate Response'];
-
-                buttons.forEach((button, index) => {
-                    if (index < labels.length && !button.hasAttribute('aria-label')) {
-                        button.setAttribute('aria-label', labels[index]);
-                    }
-                });
+        buttons.forEach((button, index) => {
+            if (index < labels.length && !button.hasAttribute('aria-label')) {
+                button.setAttribute('aria-label', labels[index]);
             }
-        }
-    }
+        });
+        console.log('Buttons updated with ARIA labels.');
+    });
 }
 
-if (targetNode) {
-    const observer = new MutationObserver(mutations => {
-        mutations.forEach(mutation => {
-            if (mutation.addedNodes.length) {
-                replaceDivsWithH3();
-                addAriaLabelsToButtons();
+let responseAnnounced = false;
+let secondaryObserverIsActive = false;
+
+function announceMessage(message) {
+    liveRegion.textContent = message;
+    setTimeout(() => {
+        liveRegion.textContent = '';
+    }, 100);
+    console.log(`Announced message '${message}'`);
+}
+
+function announceResponseFinished() {
+    announceMessage('Response is finished.');
+    responseAnnounced = true;
+    console.log('Announced text and set responseAnnounced to true.');
+}
+
+function observeChatGptNode(node) {
+    const secondaryObserver = new MutationObserver((mutations, obs) => {
+        console.log('Secondary observer triggered.');
+        addAriaLabelsToButtons(node);
+        if (responseIsFinished(node)) {
+            console.log('Response is finished.');
+            announceResponseFinished();
+            obs.disconnect();
+            secondaryObserverIsActive = false;
+        } else {
+            console.log('Response not finished.');
+        }
+    });
+
+    secondaryObserver.observe(node, { childList: true, subtree: true });
+    secondaryObserverIsActive = true;
+    console.log(`Observing ChatGPT message with class = '${node.classList.value}'`);
+
+    // Set a timeout to disconnect the observer if the maximum wait time is exceeded
+    setTimeout(() => {
+        if (secondaryObserverIsActive) {
+            secondaryObserver.disconnect();
+            secondaryObserverIsActive = false;
+
+            announceMessage('The wait time for ChatGPT to finish responding has been reached.');
+            responseAnnounced = true;
+            console.log('Timeout for ChatGPT message observer was reached. Disconnecting observer.');
+        }
+    }, MAX_WAIT_TIME);
+}
+
+// Add a live region for announcements
+const liveRegion = document.createElement('div');
+liveRegion.setAttribute('role', 'status');
+liveRegion.setAttribute('aria-live', 'polite');
+liveRegion.setAttribute('aria-atomic', 'true');
+liveRegion.style.position = 'absolute';
+liveRegion.style.width = '1px';
+liveRegion.style.height = '1px';
+liveRegion.style.margin = '-1px';
+liveRegion.style.padding = '0';
+liveRegion.style.overflow = 'hidden';
+liveRegion.style.clip = 'rect(0, 0, 0, 0)';
+liveRegion.style.whiteSpace = 'nowrap';
+liveRegion.style.border = '0';
+document.body.appendChild(liveRegion);
+console.log('Created live region and added it to the document model.');
+
+const observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+        mutation.addedNodes.forEach(node => {
+            if (isChatGptMessage(node)) {
+                announceMessage('ChatGpt is generating a response. Please stand by.');
+                console.log('Received ChatGPT message.');
+                replaceChatGptDivWithH3(node);
+                observeChatGptNode(node);
+            } else if (isUserMessage(node)) {
+                console.log('User\'s message was sent.');
+                responseAnnounced = false;
+                console.log('Set responseAnnounced to false.');
             }
         });
     });
+});
 
-    const config = { childList: true, subtree: true };
+const targetNode = document.querySelector('main');
 
+const config = { childList: true, subtree: true };
+
+if (targetNode) {
+    // Attempt to apply accessibility enhancements to any existing items if the user might have loaded a previous chat session.
+    // Give it a few moments to load up.
+    setTimeout(() => {
+        replaceChatGptDivWithH3(targetNode);
+        addAriaLabelsToButtons(targetNode);
+    }, 2000);
+
+    console.log('Target node for observation acquired. Beginning observation.');
     observer.observe(targetNode, config);
-    replaceDivsWithH3(); // Run initially for existing elements
-    addAriaLabelsToButtons();
 } else {
-    console.log('Target node (main) not found');
+    console.log('Target node not found');
 }
